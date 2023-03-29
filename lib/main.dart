66f15169327cc9d4048a38579b64cc81492263d7
daviiid99 <davidmolinamorales@gmail.com>
2023-03-29 13:02:53 +0200

    Check if assets exists to prompt users to login screen or downloader screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,43 @@
+import 'dart:io';
+
 import 'package:animal_crossing_radio/LoginScreen/loginScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'Screens/screen.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(mainApp());
+void main() async  {
+
+  if (Directory("data/user/0/com.daviiid99.ac_radio/app_flutter/songs/gc").existsSync()
+      && Directory("data/user/0/com.daviiid99.ac_radio/app_flutter/songs/ds").existsSync()
+      && Directory("data/user/0/com.daviiid99.ac_radio/app_flutter/songs/wii").existsSync()
+      && Directory("data/user/0/com.daviiid99.ac_radio/app_flutter/songs/3ds").existsSync()
+      && Directory("data/user/0/com.daviiid99.ac_radio/app_flutter/songs/switch").existsSync()){
+    runApp(Login());
+  } else {
+    runApp(Downloader());
+  }
 }
 
 
-class mainApp extends StatelessWidget{
+class Login extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-          home: DownloadView(),
-          debugShowCheckedModeBanner: false,
-          debugShowMaterialGrid: false,
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+    );
+  }
+}
+
+class Downloader extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: DownloadView(),
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
     );
   }
 }
